77a435033c674e59ca3fa40a60c8126008a7f182
user <eremtroy@gmail.com>
2021-03-14 00:53:05 +0100

    move serverInfo and docs to index.js instead of src/routes/index.js

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const {
   NODE_ENV,
   HOST,
   PORT,
+  npm_package_name: name,
+  npm_package_version: version,
 } = process.env
 const isDev = NODE_ENV === 'development'
+
+const serverInfo = {
+  name,
+  version,
+}
+if (isDev) serverInfo.documentation = `http://${HOST}:${PORT}/documentation`
+server.get('/', (req, res) => {
+  res.json(serverInfo)
+})
+
+if (isDev) {
+  const swaggerUi = require('swagger-ui-express')
+  const swagger = require('./swagger')
+  server.use('/documentation', swaggerUi.serve, swaggerUi.setup(swagger))
+}
+
+server.use((req, res) => res.status(404).json({ code: 404, message: 'Not found.' }))
 
 server.listen(PORT, HOST, () => {
   if (isDev) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,3 @@
 const router = require('express').Router()
 
-const {
-  npm_package_name: name,
-  npm_package_version: version,
-} = process.env
-
-router.get('/', (req, res) => {
-  res.json({
-    name,
-    version,
-  })
-})
-
-const isDev = process.env.NODE_ENV === 'development'
-if (isDev) {
-  const swaggerUi = require('swagger-ui-express')
-  const swagger = require('../../swagger')
-  router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swagger))
-}
-
-router.use((req, res) => res.status(404).json({ code: 404, message: 'Not found.' }))
-
 module.exports = router
