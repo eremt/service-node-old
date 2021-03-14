d83934cdef45e293e08675372ef8908cfc3fd3c1
user <eremtroy@gmail.com>
2021-03-14 01:01:14 +0100

    last few changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
   NODE_ENV,
   HOST,
   PORT,
+  DOCS,
   npm_package_name: name,
   npm_package_version: version,
 } = process.env
@@ -13,7 +14,9 @@ const serverInfo = {
   name,
   version,
 }
-if (isDev) serverInfo.documentation = `http://${HOST}:${PORT}/documentation`
+const urlDocumentation = `http://${HOST}:${PORT}${DOCS}`
+if (isDev) serverInfo.documentation = urlDocumentation
+
 server.get('/', (req, res) => {
   res.json(serverInfo)
 })
@@ -29,6 +32,6 @@ server.use((req, res) => res.status(404).json({ code: 404, message: 'Not found.'
 server.listen(PORT, HOST, () => {
   if (isDev) {
     console.log(`Server running on http://${HOST}:${PORT}`)
-    console.log(`Documentation running on http://${HOST}:${PORT}/documentation`)
+    console.log(`Documentation running on ${urlDocumentation}`)
   }
 })
