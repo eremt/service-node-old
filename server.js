7c6097f2f8fcb357cc1d52279d9dd95f78c9602a
user <eremtroy@gmail.com>
2021-03-15 01:35:01 +0100

    wip: SessionService.middleware factory

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const isDev = process.env.NODE_ENV === 'development'
 if (!isDev) {
   // TODO: for now disable helmet in dev because it breaks swagger, investigate
   const helmet = require('helmet')
-  server.use(helmet({
-    referrerPolicy: false,
-  }))
+  server.use(helmet())
 }
 
 const bodyParser = require('body-parser')
@@ -18,6 +16,9 @@ server.use(bodyParser.json())
 
 const cookieParser = require('cookie-parser')
 server.use(cookieParser())
+
+const { SessionService } = require('./src/services')
+server.use(SessionService.middleware())
 
 const { logRequests } = require('./src/utils/logger')
 server.use(logRequests())
--- src/services/session.service.js
+++ src/services/session.service.js
@@ -1,5 +1,13 @@
 const redis = require('../redis')
+const { v4: uuid } = require('uuid')
 const { log } = require('../utils/logger')
+
+const { NODE_ENV } = process.env
+const cookieConfig = {
+  httpOnly: true,
+  maxAge: 1000 * 60,
+  secure: NODE_ENV !== 'development',
+}
 
 class SessionService {
   static async get (id) {
@@ -25,6 +33,20 @@ class SessionService {
       return redis.del(id)
     } catch (error) {
       log(error)
+    }
+  }
+
+  static middleware (options = { ...cookieConfig }) {
+    return async function (req, res, next) {
+      let { token } = req.cookies
+
+      if (!token) {
+        token = uuid()
+        await SessionService.set(token, {})
+        res.cookie('token', token, options)
+      }
+
+      next()
     }
   }
 }
