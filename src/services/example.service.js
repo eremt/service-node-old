d7149352d63279d9ff559c42af85febffecf6098
user <eremtroy@gmail.com>
2021-03-15 22:35:07 +0100

    mock ExampleService

--- src/services/example.service.js
+++ src/services/example.service.js
@@ -1,4 +1,25 @@
+const example = {
+  id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  name: 'Example',
+  description: 'An example',
+}
+
 class ExampleService {
+  static async getExample () {
+    return example
+  }
+
+  static async createExample () {
+    return example
+  }
+
+  static async updateExample () {
+    return example
+  }
+
+  static async deleteExample () {
+    return example
+  }
 }
 
 module.exports = ExampleService
