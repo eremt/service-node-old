46b34b50e8f435c0bf3af9f5b41fb8c74d691396
user <eremtroy@gmail.com>
2021-03-16 01:43:31 +0100

    rename routes/example.js -> routes/example.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const example = require('./example')
+const example = require('./example.routes')
 router.use('/examples', example)
 
 module.exports = router
