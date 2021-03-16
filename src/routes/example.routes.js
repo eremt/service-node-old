e838f73d8d946b015c9c631fa86afc873bd21a4c
user <eremtroy@gmail.com>
2021-03-16 02:06:00 +0100

    change routes order to match controller and service

--- src/routes/example.routes.js
+++ src/routes/example.routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const { ExampleController } = require('../controllers')
 
-router.get('/', ExampleController.getExamples)
 router.get('/:id', ExampleController.getExample)
+router.get('/', ExampleController.getExamples)
 router.post('/', ExampleController.createExample)
 router.put('/:id', ExampleController.updateExample)
 router.delete('/:id', ExampleController.deleteExample)
